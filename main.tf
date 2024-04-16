15187347dc2cf954024639a71b0f0cd1900759c5
lokiharsha <44457615+lokiharsha@users.noreply.github.com>
2024-04-16 20:08:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+prvider "aws"{
+  region="us-east-1"
+  profile="default"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-for-cloud"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
